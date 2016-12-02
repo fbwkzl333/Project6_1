c59079346aad1eff9c41ac1affe6e390e5309c21
fbwkzl333 <fbwkzl111@naver.com>
2016-12-02 11:22:29 +0900

    2. [예약시작] 버튼을 클릭하면 그 위의 Chronometer가 작동한다.

--- app/src/main/java/com/example/donghee/project6_1/MainActivity.java
+++ app/src/main/java/com/example/donghee/project6_1/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.donghee.project6_1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn1, btn2;
+    RadioButton rd1, rd2;
+    CalendarView cal;
+    TimePicker timeP;
+    TextView tview;
+    Chronometer chro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = (Button) findViewById(R.id.button);      // 예약시작 버튼
+        btn2 = (Button) findViewById(R.id.button3);
+        rd1 = (RadioButton) findViewById(R.id.radioButton1);
+        rd2 = (RadioButton) findViewById(R.id.radioButton2);
+        cal = (CalendarView) findViewById(R.id.calendarView11);
+        timeP = (TimePicker) findViewById(R.id.timePicker3);
+        tview = (TextView) findViewById(R.id.textView1);
+        chro = (Chronometer) findViewById(R.id.chronometer);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chro.setBase(SystemClock.elapsedRealtime());
+                chro.start();
+                chro.setTextColor(Color.RED);
+
+            }
+        });
     }
 }
